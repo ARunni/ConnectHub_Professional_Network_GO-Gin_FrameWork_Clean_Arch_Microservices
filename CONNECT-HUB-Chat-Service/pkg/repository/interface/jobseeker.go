4e832e95b36ef656a82827e66aae1581ec4764e9
Arun <aruncmunni@gmail.com>
2024-06-03 10:32:01 +0530

    updated chat session

--- CONNECT-HUB-Chat-Service/pkg/repository/interface/jobseeker.go
+++ CONNECT-HUB-Chat-Service/pkg/repository/interface/jobseeker.go
@@ -1,9 +1,1 @@
 package interfaces
-
-import "ConnetHub_chat/pkg/utils/models"
-
-type ChatRepository interface {
-	StoreFriendsChat(models.MessageReq) error
-	GetFriendChat(string, string, models.Pagination) ([]models.Message, error)
-	UpdateReadAsMessage(string, string) error
-}
