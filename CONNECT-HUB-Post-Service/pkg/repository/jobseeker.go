83f9c3bc1faf1d7e5071ff2d342d8f039a482abf
Arun <aruncmunni@gmail.com>
2024-06-20 22:26:07 +0530

    updated logging

--- CONNECT-HUB-Post-Service/pkg/repository/jobseeker.go
+++ CONNECT-HUB-Post-Service/pkg/repository/jobseeker.go
@@ -1,11 +1,12 @@
 package repository
 
 import (
+	"os"
+	"time"
+
 	logging "github.com/ARunni/ConnetHub_post/Logging"
 	"github.com/ARunni/ConnetHub_post/pkg/repository/interfaces"
 	"github.com/ARunni/ConnetHub_post/pkg/utils/models"
-	"os"
-	"time"
 
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -27,6 +28,7 @@ func NewJobseekerPostRepository(DB *gorm.DB) interfaces.JobseekerPostRepository 
 }
 
 func (jr *jobseekerPostRepository) CreatePost(post models.CreatePostRes) (models.CreatePostRes, error) {
+	jr.Logger.Info("CreatePost at jobseekerPostRepository started")
 
 	var res models.CreatePostRes
 	query := `
@@ -39,12 +41,15 @@ func (jr *jobseekerPostRepository) CreatePost(post models.CreatePostRes) (models
 	row := jr.DB.Raw(query, post.Title, post.Content, post.ImageUrl, post.JobseekerId, time.Now()).Row()
 	err := row.Scan(&res.ID, &res.Title, &res.Content, &res.ImageUrl, &res.JobseekerId, &res.CreatedAt)
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return models.CreatePostRes{}, err
 	}
+	jr.Logger.Info("CreatePost at jobseekerPostRepository finished")
 	return res, nil
 }
 
 func (jr *jobseekerPostRepository) GetOnePost(postId int) (models.CreatePostResp, error) {
+	jr.Logger.Info("GetOnePost at jobseekerPostRepository started")
 	var res models.CreatePostResp
 	querry := `
 		select id,jobseeker_id,title,content,image_url,created_at 
@@ -53,12 +58,15 @@ func (jr *jobseekerPostRepository) GetOnePost(postId int) (models.CreatePostResp
 
 	err := jr.DB.Raw(querry, postId).Scan(&res).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return models.CreatePostResp{}, err
 	}
+	jr.Logger.Info("GetOnePost at jobseekerPostRepository finished")
 	return res, nil
 }
 
 func (jr *jobseekerPostRepository) GetAllPost() (models.AllPost, error) {
+	jr.Logger.Info("GetAllPost at jobseekerPostRepository started")
 	var res []models.CreatePostResp
 	querry := `
 		select id,jobseeker_id,title,content,image_url,created_at
@@ -67,17 +75,20 @@ func (jr *jobseekerPostRepository) GetAllPost() (models.AllPost, error) {
 
 	err := jr.DB.Raw(querry).Scan(&res).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return models.AllPost{}, err
 	}
 	allPosts := models.AllPost{
 		Posts: res,
 	}
+	jr.Logger.Info("GetAllPost at jobseekerPostRepository finished")
 	return allPosts, nil
 }
 
 //
 
 func (jr *jobseekerPostRepository) UpdatePost(post models.EditPostRes) (models.EditPostRes, error) {
+	jr.Logger.Info("UpdatePost at jobseekerPostRepository started")
 	var res models.EditPostRes
 	querry := `
 		update posts set title = $1,content = $2,image_url = $3,created_at = $4 
@@ -86,25 +97,30 @@ func (jr *jobseekerPostRepository) UpdatePost(post models.EditPostRes) (models.E
 
 	err := jr.DB.Raw(querry, post.Title, post.Content, post.ImageUrl, time.Now(), post.PostId, post.JobseekerId).Scan(&res).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return models.EditPostRes{}, err
 	}
+	jr.Logger.Info("UpdatePost at jobseekerPostRepository finished")
 	return res, nil
 }
 
 func (jr *jobseekerPostRepository) DeletePost(postId, JobseekerId int) (bool, error) {
-
+	jr.Logger.Info("DeletePost at jobseekerPostRepository started")
 	querry := `
 	delete from posts 
 	where id = ? and jobseeker_id = ?
 	`
 	err := jr.DB.Exec(querry, postId, JobseekerId).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("DeletePost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) IsPostExistByPostId(postId int) (bool, error) {
+	jr.Logger.Info("IsPostExistByPostId at jobseekerPostRepository started")
 	var ok int
 	querry := `
 	select count(*) from posts 
@@ -112,12 +128,15 @@ func (jr *jobseekerPostRepository) IsPostExistByPostId(postId int) (bool, error)
 	`
 	err := jr.DB.Raw(querry, postId).Scan(&ok).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("IsPostExistByPostId at jobseekerPostRepository finished")
 	return ok > 0, nil
 }
 
 func (jr *jobseekerPostRepository) IsPostExistByUserId(userId int) (bool, error) {
+	jr.Logger.Info("IsPostExistByUserId at jobseekerPostRepository started")
 	var ok int
 	querry := `
 	select count(*) from posts 
@@ -125,88 +144,115 @@ func (jr *jobseekerPostRepository) IsPostExistByUserId(userId int) (bool, error)
 	`
 	err := jr.DB.Raw(querry, userId).Scan(&ok).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("IsPostExistByUserId at jobseekerPostRepository finished")
 	return ok > 0, nil
 }
 
 func (jr *jobseekerPostRepository) CreateCommentPost(postId, userId int, comment string) (bool, error) {
+	jr.Logger.Info("CreateCommentPost at jobseekerPostRepository started")
 	querry := `insert into comments (post_id,comment,jobseeker_id,created_at)
 	values ($1,$2,$3,$4)`
 	err := jr.DB.Exec(querry, postId, comment, userId, time.Now()).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("CreateCommentPost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) IsCommentIdExist(commentId int) (bool, error) {
+	jr.Logger.Info("IsCommentIdExist at jobseekerPostRepository started")
 	var ok int
 	querry := `select count(*) from comments where id = ?`
 	err := jr.DB.Raw(querry, commentId).Scan(&ok).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("IsCommentIdExist at jobseekerPostRepository finished")
 	return ok > 0, nil
 }
 
 func (jr *jobseekerPostRepository) IsCommentIdBelongsUserId(commentId, userId int) (bool, error) {
+	jr.Logger.Info("IsCommentIdBelongsUserId at jobseekerPostRepository started")
 	var ok int
 	querry := `select count(*) from comments where id = ? and  jobseeker_id = ?`
 	err := jr.DB.Raw(querry, commentId, userId).Scan(&ok).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("IsCommentIdBelongsUserId at jobseekerPostRepository finished")
 	return ok > 0, nil
 }
 
 func (jr *jobseekerPostRepository) UpdateCommentPost(commentId, postId, userId int, comment string) (bool, error) {
+	jr.Logger.Info("UpdateCommentPost at jobseekerPostRepository started")
 	querry := `update comments set comment = ?, updated_at = ? where id = ? and  jobseeker_id = ? and post_id = ? `
 	err := jr.DB.Exec(querry, comment, time.Now(), commentId, userId, postId).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("UpdateCommentPost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) DeleteCommentPost(postId, userId, commentId int) (bool, error) {
+	jr.Logger.Info("DeleteCommentPost at jobseekerPostRepository started")
 	querry := `delete from comments where id = ? and  jobseeker_id = ? and post_id = ? `
 	err := jr.DB.Exec(querry, commentId, userId, postId).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("DeleteCommentPost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) IsLikeExist(postId, userId int) (bool, error) {
+	jr.Logger.Info("IsLikeExist at jobseekerPostRepository started")
 	var ok int
 	querry := `select count(*) from likes where jobseeker_id = ? and post_id = ? `
 	err := jr.DB.Raw(querry, userId, postId).Scan(&ok).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("IsLikeExist at jobseekerPostRepository finished")
 	return ok > 0, nil
 }
 
 func (jr *jobseekerPostRepository) AddLikePost(postId, userId int) (bool, error) {
+	jr.Logger.Info("AddLikePost at jobseekerPostRepository started")
 	querry := `insert into likes (jobseeker_id,post_id,created_at) values (?,?,?)`
 	err := jr.DB.Exec(querry, userId, postId, time.Now()).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("AddLikePost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) RemoveLikePost(postId, userId int) (bool, error) {
+	jr.Logger.Info("RemoveLikePost at jobseekerPostRepository started")
 	querry := `delete from likes where post_id = ? and  jobseeker_id = ?`
 	err := jr.DB.Exec(querry, postId, userId).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return false, err
 	}
+	jr.Logger.Info("RemoveLikePost at jobseekerPostRepository finished")
 	return true, nil
 }
 
 func (jr *jobseekerPostRepository) GetCommentsPost(postId int) ([]models.CommentData, error) {
+	jr.Logger.Info("GetCommentsPost at jobseekerPostRepository started")
 
 	var comments []models.CommentData
 
@@ -214,18 +260,23 @@ func (jr *jobseekerPostRepository) GetCommentsPost(postId int) ([]models.Comment
 	from comments where post_id = ?`
 	err := jr.DB.Raw(querry, postId).Scan(&comments).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return nil, err
 	}
+	jr.Logger.Info("GetCommentsPost at jobseekerPostRepository finished")
 	return comments, nil
 }
 
 func (jr *jobseekerPostRepository) GetLikesCountPost(postId int) (int, error) {
+	jr.Logger.Info("GetLikesCountPost at jobseekerPostRepository started")
 	var count int
 	query := `SELECT count(*) 
 	FROM likes WHERE post_id = ?`
 	err := jr.DB.Raw(query, postId).Scan(&count).Error
 	if err != nil {
+		jr.Logger.Error("error at database", err)
 		return 0, err
 	}
+	jr.Logger.Info("GetLikesCountPost at jobseekerPostRepository finished")
 	return count, nil
 }
--- CONNECT-HUB-Post-Service/pkg/usecase/jobseeker.go
+++ CONNECT-HUB-Post-Service/pkg/usecase/jobseeker.go
@@ -34,57 +34,77 @@ func NewJobseekerpostUseCase(repo repo.JobseekerPostRepository, client auth.Newa
 }
 
 func (ju *jobseekerJobUseCase) CreatePost(post models.CreatePostReq) (models.CreatePostRes, error) {
+	ju.Logger.Info("CreatePost at jobseekerJobUseCase started")
 
 	var postReq models.CreatePostRes
 
 	cfg, err := config.LoadConfig()
 	if err != nil {
+		ju.Logger.Error("error from LoadConfig", err)
 		return models.CreatePostRes{}, err
 	}
 
 	h := helper.NewHelper(cfg)
+	ju.Logger.Info("AddImageToAwsS3 started")
 	url, err := h.AddImageToAwsS3(post.Image)
 	if err != nil {
+		ju.Logger.Error("error from AddImageToAwsS3", err)
 		return models.CreatePostRes{}, err
 	}
-
+	ju.Logger.Info("AddImageToAwsS3 finished")
 	postReq.Content = post.Content
 	postReq.Title = post.Title
 	postReq.JobseekerId = post.JobseekerId
 	postReq.ImageUrl = url
-
+	ju.Logger.Info("CreatePost at postRepository started")
 	postData, err := ju.postRepository.CreatePost(postReq)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.CreatePostRes{}, err
 	}
+	ju.Logger.Info("CreatePost at postRepository finished")
+	ju.Logger.Info("CreatePost at jobseekerJobUseCase finished")
 	return postData, nil
 
 }
 
 func (ju *jobseekerJobUseCase) GetOnePost(postId int) (models.CreatePostRes, error) {
-
+	ju.Logger.Info("GetOnePost at jobseekerJobUseCase started")
+	ju.Logger.Info("IsPostExistByPostId at postRepository started")
 	ok, err := ju.postRepository.IsPostExistByPostId(postId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.CreatePostRes{}, err
 	}
-
+	ju.Logger.Info("IsPostExistByPostId at postRepository finished")
 	if !ok {
+		ju.Logger.Error("error : post does not exist")
 		return models.CreatePostRes{}, errors.New("post does not exist")
 	}
+	ju.Logger.Info("GetOnePost at postRepository started")
 	postData, err := ju.postRepository.GetOnePost(postId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.CreatePostRes{}, err
 	}
+	ju.Logger.Info("GetOnePost at postRepository finished")
+	ju.Logger.Info("GetLikesCountPost at postRepository started")
 	likeData, err := ju.postRepository.GetLikesCountPost(postId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.CreatePostRes{}, err
 	}
+	ju.Logger.Info("GetLikesCountPost at postRepository finished")
+	ju.Logger.Info("GetCommentsPost at postRepository started")
+
 	commentData, err := ju.postRepository.GetCommentsPost(postId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.CreatePostRes{}, err
 	}
+	ju.Logger.Info("GetCommentsPost at postRepository finished")
 	var result = models.CreatePostRes{
 		ID:          postData.ID,
 		JobseekerId: postData.JobseekerId,
@@ -97,29 +117,36 @@ func (ju *jobseekerJobUseCase) GetOnePost(postId int) (models.CreatePostRes, err
 	}
 	// postData.Likes = likeData
 	// postData.Comments = commentData
-
+	ju.Logger.Info("GetOnePost at jobseekerJobUseCase finished")
 	return result, nil
 }
 
 func (ju *jobseekerJobUseCase) GetAllPost() (models.AllPostData, error) {
-
+	ju.Logger.Info("GetAllPost at jobseekerJobUseCase started")
+	ju.Logger.Info("GetAllPost at postRepository started")
 	postData, err := ju.postRepository.GetAllPost()
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.AllPostData{}, err
 	}
-
+	ju.Logger.Info("GetAllPost at postRepository finished")
 	var postAllData []models.CreatePostRes
 
 	for _, post := range postData.Posts {
+		ju.Logger.Info("GetLikesCountPost at postRepository started")
 		likeData, err := ju.postRepository.GetLikesCountPost(post.ID)
 		if err != nil {
+			ju.Logger.Error("error from postRepository", err)
 			return models.AllPostData{}, err
 		}
+		ju.Logger.Info("GetLikesCountPost at postRepository finished")
+		ju.Logger.Info("GetCommentsPost at postRepository started")
 		commentData, err := ju.postRepository.GetCommentsPost(post.ID)
 		if err != nil {
+			ju.Logger.Error("error from postRepository", err)
 			return models.AllPostData{}, err
 		}
-
+		ju.Logger.Info("GetCommentsPost at postRepository finished")
 		postAllData = append(postAllData, models.CreatePostRes{
 			ID:          post.ID,
 			JobseekerId: post.JobseekerId,
@@ -131,43 +158,55 @@ func (ju *jobseekerJobUseCase) GetAllPost() (models.AllPostData, error) {
 			CreatedAt:   post.CreatedAt,
 		})
 	}
-
+	ju.Logger.Info("GetAllPost at jobseekerJobUseCase finished")
 	return models.AllPostData{Posts: postAllData}, nil
 }
 
 //
 
 func (ju *jobseekerJobUseCase) UpdatePost(post models.EditPostReq) (models.EditPostRes, error) {
-
+	ju.Logger.Info("UpdatePost at jobseekerJobUseCase started")
+	ju.Logger.Info("IsPostExistByPostId at postRepository started")
 	okp, err := ju.postRepository.IsPostExistByPostId(post.PostId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.EditPostRes{}, err
 	}
-
+	ju.Logger.Info("IsPostExistByPostId at postRepository finished")
 	if !okp {
+		ju.Logger.Error("error : post does not exist")
 		return models.EditPostRes{}, errors.New("post does not exist")
 	}
-
+	ju.Logger.Info("IsPostExistByUserId at postRepository started")
 	oku, err := ju.postRepository.IsPostExistByUserId(post.JobseekerId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.EditPostRes{}, err
 	}
-
+	ju.Logger.Info("IsPostExistByUserId at postRepository finished")
 	if !oku {
+		ju.Logger.Error("error : post does not belongs to you")
 		return models.EditPostRes{}, errors.New("post does not belongs to you")
 	}
+	ju.Logger.Info("LoadConfig at config started")
 	cfg, err := config.LoadConfig()
 	if err != nil {
+		ju.Logger.Error("error from LoadConfig", err)
 		return models.EditPostRes{}, err
 	}
-
+	ju.Logger.Info("LoadConfig at config finished")
+	ju.Logger.Info("NewHelper at helper started")
 	h := helper.NewHelper(cfg)
+	ju.Logger.Info("NewHelper at helper finished")
+	ju.Logger.Info("AddImageToAwsS3 started")
 	url, err := h.AddImageToAwsS3(post.Image)
 	if err != nil {
+		ju.Logger.Error("error from AddImageToAwsS3", err)
 		return models.EditPostRes{}, err
 	}
+	ju.Logger.Info("AddImageToAwsS3 finished")
 	var editPost = models.EditPostRes{
 		JobseekerId: post.JobseekerId,
 		PostId:      uint(post.PostId),
@@ -175,69 +214,88 @@ func (ju *jobseekerJobUseCase) UpdatePost(post models.EditPostReq) (models.EditP
 		Content:     post.Content,
 		ImageUrl:    url,
 	}
+	ju.Logger.Info("UpdatePost at postRepository started")
 	PostData, err := ju.postRepository.UpdatePost(editPost)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return models.EditPostRes{}, err
 	}
-
+	ju.Logger.Info("UpdatePost at postRepository finished")
+	ju.Logger.Info("UpdatePost at jobseekerJobUseCase finished")
 	return PostData, nil
 
 }
 
 func (ju *jobseekerJobUseCase) DeletePost(postId, JobseekerId int) (bool, error) {
-
+	ju.Logger.Info("DeletePost at jobseekerJobUseCase started")
+	ju.Logger.Info("IsPostExistByPostId at postRepository started")
 	okP, err := ju.postRepository.IsPostExistByPostId(postId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsPostExistByPostId at postRepository finished")
 	if !okP {
+		ju.Logger.Error("error : post does not exist")
 		return false, errors.New("post does not exist")
 	}
-
+	ju.Logger.Info("IsPostExistByUserId at postRepository started")
 	okU, err := ju.postRepository.IsPostExistByUserId(JobseekerId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository")
 		return false, err
 	}
+	ju.Logger.Info("IsPostExistByUserId at postRepository finished")
 	if !okU {
+		ju.Logger.Error("error : post is not belongs to you")
 		return false, errors.New("post is not belongs to you")
 	}
-
+	ju.Logger.Info("DeletePost at postRepository started")
 	ok, err := ju.postRepository.DeletePost(postId, JobseekerId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
-
+	ju.Logger.Info("DeletePost at postRepository finished")
+	ju.Logger.Info("DeletePost at jobseekerJobUseCase finished")
 	return ok, nil
 
 }
 func (ju *jobseekerJobUseCase) CreateCommentPost(postId, userId int, comment string) (bool, error) {
-
+	ju.Logger.Info("CreateCommentPost at jobseekerJobUseCase started")
 	if postId <= 0 {
+		ju.Logger.Error("error : postId is required")
 		return false, errors.New("postId is required")
 	}
 	if comment == "" {
+		ju.Logger.Error("error : comment is required")
 		return false, errors.New("comment is required")
 	}
-
+	ju.Logger.Info("IsPostExistByPostId at postRepository started")
 	okP, err := ju.postRepository.IsPostExistByPostId(postId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsPostExistByPostId at postRepository finished")
 	if !okP {
+		ju.Logger.Error("error : post does not exist")
 		return false, errors.New("post does not exist")
 	}
+	ju.Logger.Info("CreateCommentPost at postRepository started")
 	ok, err := ju.postRepository.CreateCommentPost(postId, userId, comment)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
-
+	ju.Logger.Info("CreateCommentPost at postRepository finished")
 	//
-
+	ju.Logger.Info("UserData at authClient started")
 	senderData, err := ju.authClient.UserData(userId)
 	if err != nil {
 		ju.Logger.Error("error from authClient", err)
@@ -261,84 +319,116 @@ func (ju *jobseekerJobUseCase) CreateCommentPost(postId, userId int, comment str
 }
 
 func (ju *jobseekerJobUseCase) UpdateCommentPost(commentId, postId, userId int, comment string) (bool, error) {
+	ju.Logger.Info("UpdateCommentPost at jobseekerJobUseCase started")
 	if postId <= 0 {
+		ju.Logger.Error("error : postId is required")
 		return false, errors.New("postId is required")
 	}
 	if comment == "" {
+		ju.Logger.Error("error : comment is required")
 		return false, errors.New("comment is required")
 	}
 	if commentId <= 0 {
+		ju.Logger.Error("error : commentId is required")
 		return false, errors.New("commentId is required")
 	}
-
+	ju.Logger.Info("IsCommentIdExist at postRepository started")
 	okc, err := ju.postRepository.IsCommentIdExist(commentId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsCommentIdExist at postRepository finished")
 	if !okc {
+		ju.Logger.Error("error : comment does not exist")
 		return false, errors.New("comment does not exist")
 	}
-
+	ju.Logger.Info("IsCommentIdBelongsUserId at postRepository started")
 	okcu, err := ju.postRepository.IsCommentIdBelongsUserId(commentId, userId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsCommentIdBelongsUserId at postRepository finished")
 	if !okcu {
+		ju.Logger.Error("error : comment does not belongs to you")
 		return false, errors.New("comment does not belongs to you")
 	}
+	ju.Logger.Info("UpdateCommentPost at postRepository started")
 	ok, err := ju.postRepository.UpdateCommentPost(commentId, postId, userId, comment)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
-
+	ju.Logger.Info("UpdateCommentPost at postRepository finished")
+	ju.Logger.Info("UserData at authClient started")
 	senderData, err := ju.authClient.UserData(userId)
 	if err != nil {
 		ju.Logger.Error("error from authClient", err)
 		return false, err
 	}
+	ju.Logger.Info("UserData at authClient finished")
+	ju.Logger.Info("GetOnePost at postRepository started")
 	postUser, err := ju.postRepository.GetOnePost(postId)
 	if err != nil {
 		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("GetOnePost at postRepository finished")
 	msg := fmt.Sprintf("%s comment edited on Your PostID %d", senderData.Username, postId)
+	ju.Logger.Info("SendNotification at helper started")
 	helper.SendNotification(models.Notification{
 		UserID:     postUser.JobseekerId,
 		SenderID:   senderData.UserId,
 		PostID:     postId,
 		SenderName: senderData.Username,
 	}, []byte(msg))
-
+	ju.Logger.Info("SendNotification at helper finished")
+	ju.Logger.Info("UpdateCommentPost at jobseekerJobUseCase finished")
 	return ok, nil
 
 }
 
 func (ju *jobseekerJobUseCase) DeleteCommentPost(postId, userId, commentId int) (bool, error) {
+	ju.Logger.Info("DeleteCommentPost at jobseekerJobUseCase started")
 	if postId <= 0 {
+		ju.Logger.Error("error : postId is required")
 		return false, errors.New("postId is required")
 	}
 	if commentId <= 0 {
+		ju.Logger.Error("error : commentId is required")
 		return false, errors.New("commentId is required")
 	}
+	ju.Logger.Info("IsCommentIdExist at postRepository started")
 	okc, err := ju.postRepository.IsCommentIdExist(commentId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsCommentIdExist at postRepository finished")
 	if !okc {
+		ju.Logger.Error("error : comment does not exist")
 		return false, errors.New("comment does not exist")
 	}
-
+	ju.Logger.Info("IsCommentIdBelongsUserId at postRepository started")
 	okcu, err := ju.postRepository.IsCommentIdBelongsUserId(commentId, userId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsCommentIdBelongsUserId at postRepository finished")
 	if !okcu {
+		ju.Logger.Error("error : comment does not belongs to you")
 		return false, errors.New("comment does not belongs to you")
 	}
+	ju.Logger.Info("DeleteCommentPost at postRepository started")
 	ok, err := ju.postRepository.DeleteCommentPost(commentId, postId, userId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("DeleteCommentPost at postRepository finished")
+	ju.Logger.Info("DeleteCommentPost at jobseekerJobUseCase finished")
 	return ok, nil
 }
 
@@ -394,24 +484,37 @@ func (ju *jobseekerJobUseCase) AddLikePost(postId, userId int) (bool, error) {
 }
 
 func (ju *jobseekerJobUseCase) RemoveLikePost(postId, userId int) (bool, error) {
+	ju.Logger.Info("RemoveLikePost at jobseekerJobUseCase started")
+	ju.Logger.Info("IsPostExistByPostId at postRepository started")
 	okP, err := ju.postRepository.IsPostExistByPostId(postId)
 
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsPostExistByPostId at postRepository finished")
 	if !okP {
+		ju.Logger.Error("error : post does not exist")
 		return false, errors.New("post does not exist")
 	}
+	ju.Logger.Info("IsLikeExist at postRepository started")
 	okL, err := ju.postRepository.IsLikeExist(postId, userId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("IsLikeExist at postRepository finished")
 	if !okL {
+		ju.Logger.Error("error : you already unliked this post")
 		return false, errors.New("you already unliked this post")
 	}
+	ju.Logger.Info("RemoveLikePost at postRepository started")
 	ok, err := ju.postRepository.RemoveLikePost(postId, userId)
 	if err != nil {
+		ju.Logger.Error("error from postRepository", err)
 		return false, err
 	}
+	ju.Logger.Info("RemoveLikePost at postRepository finished")
+	ju.Logger.Info("RemoveLikePost at jobseekerJobUseCase finished")
 	return ok, nil
 }
